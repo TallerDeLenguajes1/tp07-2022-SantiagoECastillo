16a0f859c62d7fc740cb9d123168b825cdf58f72
SantiagoECastillo <castillosantiagoe1998@gmail.com>
2022-05-31 15:56:29 -0300

    Prime commit, carga en lista y lectura en lista

---
+++ TP7/Program.cs
@@ -0,0 +1,33 @@
+﻿
+// See https://aka.ms/new-console-template for more information
+List<tarea> listaTareasPediente = new List<tarea>();
+
+int ID=1;
+string descripcion;
+int duracion;
+var numeroTareas = new Random().Next(1, 3);//Crea aleatoriamente el numero de tareas
+var numeroTareas2 = numeroTareas;
+
+while (numeroTareas > 0)
+{
+    Console.WriteLine("Ingrese la descripcion de la tarea " + ID + ": ");
+    descripcion = Console.ReadLine();
+    duracion = new Random().Next(10, 100);
+    tarea nuevaTarea = new tarea(ID, descripcion, duracion);
+    listaTareasPediente.Add(nuevaTarea);
+    //listaTareasPediente.Insert(ID, nuevaTarea);
+    ID++;
+    numeroTareas--;
+}
+
+ID = 0;
+while(numeroTareas2 != 0){
+    Console.WriteLine("---------");
+    Console.WriteLine("ID: " + listaTareasPediente[ID].TareaId);
+    Console.WriteLine("Descripcion: " + listaTareasPediente[ID].descripcion);
+    Console.WriteLine("Duracion: " + listaTareasPediente[ID].duracion);
+    numeroTareas2--;
+    ID++;
+}
+
+
---
+++ TP7/Tarea.cs
@@ -0,0 +1,15 @@
+public class tarea{
+    public int TareaId;
+    public string descripcion;
+     public int duracion; //entre 10 - 100
+
+    
+    public tarea(int TareaId, string descripcion, int duracion){
+        this.TareaId = TareaId;
+        this.descripcion = descripcion;
+        this.duracion = duracion;
+    }
+
+
+
+}
