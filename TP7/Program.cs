1a9b75245ea5468eb30457b541268b1c4d20b3e2
SantiagoECastillo <castillosantiagoe1998@gmail.com>
2022-06-30 00:07:53 -0300

    modificacion en cargo de tareas

--- TP7/Program.cs
+++ TP7/Program.cs
@@ -1,33 +1,73 @@
 ﻿
 // See https://aka.ms/new-console-template for more information
 List<tarea> listaTareasPediente = new List<tarea>();
+List<tarea> listaTareasRealizada = new List<tarea>();
 
-int ID=1;
+String salida = "";
+int ID = 1;
+Random rnd = new Random();
+//int numeroTareas = new Random().Next(1, 4);//Crea aleatoriamente el numero de tareas
+int numeroTareas = 2;
+
+do{
+    Console.WriteLine("Ingrese la descripcion de la tarea");
+    string descripcionTarea = Console.ReadLine();
+    int duracion = rnd.Next(10, 101);
+    tarea nuevaTarea = new tarea(ID, descripcionTarea, duracion);
+
+    listaTareasPediente.Add(nuevaTarea);
+
+}while(numeroTareas != 0);
+
+
+
+/*
 string descripcion;
-int duracion;
-var numeroTareas = new Random().Next(1, 3);//Crea aleatoriamente el numero de tareas
+
+
+var numeroTareas = 3;
 var numeroTareas2 = numeroTareas;
 
-while (numeroTareas > 0)
+while (numeroTareas2 > 0)
 {
     Console.WriteLine("Ingrese la descripcion de la tarea " + ID + ": ");
     descripcion = Console.ReadLine();
     duracion = new Random().Next(10, 100);
     tarea nuevaTarea = new tarea(ID, descripcion, duracion);
     listaTareasPediente.Add(nuevaTarea);
-    //listaTareasPediente.Insert(ID, nuevaTarea);
     ID++;
-    numeroTareas--;
+    numeroTareas2--;
 }
 
 ID = 0;
+numeroTareas2 = numeroTareas;
 while(numeroTareas2 != 0){
     Console.WriteLine("---------");
     Console.WriteLine("ID: " + listaTareasPediente[ID].TareaId);
     Console.WriteLine("Descripcion: " + listaTareasPediente[ID].descripcion);
     Console.WriteLine("Duracion: " + listaTareasPediente[ID].duracion);
+    Console.WriteLine("Se realizo la tarea? s/n");
+    string respuesta = Console.ReadLine();
+    if(respuesta == "s"){
+        listaTareasRealizada.Add(listaTareasPediente[ID]);
+        listaTareasPediente.RemoveAt(ID);
+    }
     numeroTareas2--;
     ID++;
 }
 
+numeroTareas2 = numeroTareas;
+for(int i=0; i<numeroTareas2; i++){
+    Console.WriteLine("---------Tareas Realizdas-------------");
+    Console.WriteLine("ID: " + listaTareasRealizada[i].TareaId);
+    Console.WriteLine("Descripcion: " + listaTareasRealizada[i].descripcion);
+    Console.WriteLine("Duracion: " + listaTareasRealizada[i].duracion);
+}
 
+numeroTareas2 = numeroTareas;
+for(int i=0; i<numeroTareas2; i++){
+    Console.WriteLine("---------Tareas Pendientes-------------");
+    Console.WriteLine("ID: " + listaTareasRealizada[i].TareaId);
+    Console.WriteLine("Descripcion: " + listaTareasRealizada[i].descripcion);
+    Console.WriteLine("Duracion: " + listaTareasRealizada[i].duracion);
+}*/
--- TP7/Tarea.cs
+++ TP7/Tarea.cs
@@ -1,15 +1,12 @@
 public class tarea{
-    public int TareaId;
-    public string descripcion;
-     public int duracion; //entre 10 - 100
+    private int tareaId;
+    private string descripcion;
+    private int duracion; //entre 10 - 100
 
     
-    public tarea(int TareaId, string descripcion, int duracion){
-        this.TareaId = TareaId;
+    public tarea(int tareaId, string descripcion, int duracion){
+        this.tareaId = tareaId;
         this.descripcion = descripcion;
         this.duracion = duracion;
     }
-
-
-
 }
